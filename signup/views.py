a2e8a1782a275c7d96463841413d2594435bb987
Brendan Leonard <bmleonard33@gmail.com>
2017-04-25 18:28:17 -0400

    Add cities to database

--- signup/views.py
+++ signup/views.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
+import os
 
 
 def index(request):
     context = {
-                    'test': True,
+                    'api_key': os.environ['WUNDERGROUND_KEY'],
                 }
 
     return render(request, 'signup/index.html', context)

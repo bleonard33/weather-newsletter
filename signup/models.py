b801788db9b1955f75c958cb6d49010943e9a7a0
Brendan Leonard <bmleonard33@gmail.com>
2017-04-25 17:48:54 -0400

    More setup work

--- signup/models.py
+++ signup/models.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    email_address = models.EmailField()
+    location = models.CharField(max_length=50)

33e9ceb5bbcbce383686557c6639abcd8b355f3c
Brendan Leonard <bmleonard33@gmail.com>
2017-04-25 23:09:45 -0400

    Wunderground API

--- weather/management/commands/send_email.py
+++ weather/management/commands/send_email.py
@@ -1,5 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from signup.models import Account
+import requests
+import os
 
 
 class Command(BaseCommand):
@@ -7,8 +9,40 @@ class Command(BaseCommand):
     WUNDERGROUND_URL = ('http://api.wunderground.com/api/{key}/'
                         '{api}/q/{state}/{city}.json')
 
-    API_ENDPOINTS = ['conditions', 'almanac']
+    WUNDERGROUND_KEY = os.environ['WUNDERGROUND_KEY']
+
+    # API_ENDPOINTS = ['conditions', 'almanac']
 
     def handle(self, *args, **options):
 
-        print [x.location.city for x in Account.objects.distinct('location')]
+        for loc in Account.objects.distinct('location'):
+
+            loc = loc.location
+
+            # Get current conditions for location
+            r = requests.get(
+                self.WUNDERGROUND_URL.format(
+                    key=self.WUNDERGROUND_KEY,
+                    api='conditions',
+                    state=loc.state,
+                    city=loc.city
+                )
+            )
+
+            curr_temp = r.json().get('current_observation').get('temp_f')
+
+            r = requests.get(
+                self.WUNDERGROUND_URL.format(
+                    key=self.WUNDERGROUND_KEY,
+                    api='almanac',
+                    state=loc.state,
+                    city=loc.city
+                )
+            )
+
+            avg_temp = (r.json().get('almanac').get('temp_high')
+                        .get('normal').get('F'))
+
+            print curr_temp, avg_temp
+
+        # print [x.location.city for x in Account.objects.distinct('location')]
